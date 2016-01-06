ab11bc5efdbcee0b5c25530bf4fa1c3fd3d710d7
Maciej Piekarski <s10739@pjwstk.edu.pl>
2016-01-06 16:17:24 +0100

    registry form dodany
    po kliknięciu załóż konto i wypełnieniu formularza do bazy dodaje się
    użytkownik

--- psw/views.py
+++ psw/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from psw.forms import CommandForm
+from psw.forms import CommandForm, ClientForm
 
 def index(request):
     context = {'boldmessage': "I am bold font from the context"}
@@ -12,13 +12,18 @@ def servers(request):
     if request.method == 'POST':
         form = CommandForm(request.POST)
         if form.is_valid():
-            
             form.save()
             return HttpResponseRedirect('/psw')
     else:
          form = CommandForm()
-         
-           
-            
-           
     return render(request, 'psw/servers.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/psw')
+    else:
+         form = ClientForm()
+    return render(request, 'psw/register.html', {'form': form})
